a2dbd24c07054224945f26d0137bddd621413105
salted7fish <1227820675@qq.com>
2021-07-01 01:46:18 +0800

    logo page finished

--- src/main/java/xyz/salted7fish/catcafe/CatCafeApplication.java
+++ src/main/java/xyz/salted7fish/catcafe/CatCafeApplication.java
@@ -2,8 +2,9 @@ package xyz.salted7fish.catcafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class CatCafeApplication {
 
     public static void main(String[] args) {
